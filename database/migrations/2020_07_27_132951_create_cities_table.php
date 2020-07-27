a256029b9f1f1268de898e193e892f56268c51a6
mazenkhalid2014 <mazenelboushy@outlook.com>
2020-07-27 15:31:54 +0200

    f

--- database/migrations/2020_07_27_132951_create_cities_table.php
+++ database/migrations/2020_07_27_132951_create_cities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class City extends Migration
+class CreateCitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class City extends Migration
      */
     public function up()
     {
-        //
-        Schema::create('city', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->string("city");
+            
             $table->timestamps();
-
         });
     }
 
@@ -29,6 +27,6 @@ class City extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('city');
+        Schema::dropIfExists('cities');
     }
 }
